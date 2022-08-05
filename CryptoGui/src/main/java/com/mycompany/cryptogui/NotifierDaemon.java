a50734e4757d6ef1e54fc6c98af3a106caea973b
Andrii Tochylin <58275791+sumerox77@users.noreply.github.com>
2022-08-05 03:20:59 +0200

    Revert "Development1"

--- CryptoGui/src/main/java/com/mycompany/cryptogui/NotifierDaemon.java
+++ CryptoGui/src/main/java/com/mycompany/cryptogui/NotifierDaemon.java
@@ -1,7 +1,8 @@
-package com.mycompany.cryptogui.notificationdaemon;
+package com.mycompany.cryptogui;
 
 import com.mycompany.cryptogui.apiclient.CoinGeckoApiClient;
 import com.mycompany.cryptogui.apiclient.constant.Currency;
+import com.mycompany.cryptogui.apiclient.impl.CoinGeckoApiClientImpl;
 import com.mycompany.cryptogui.triggers.TriggerEntity;
 import com.mycompany.cryptogui.triggers.TriggerManager;
 import dorkbox.notify.Notify;
@@ -23,14 +24,14 @@ public class NotifierDaemon implements Runnable {
             try {
                 Thread.sleep(10000);
                 if (active) {
-                    System.out.println("Notification Manager Enabled");
+                    System.out.println("me enabled");
                     synchronized (manager) {
                         for (TriggerEntity te : manager.getMap().values()) {
                             double price = client.getPrice(te.getCoinID(), Currency.USD).get(te.getCoinID()).get(Currency.USD);
                             if (price >= te.getPriceLowerBound() && price <= te.getPriceUpperBound())
                             {
                                 Notify.create()
-                                        .title("CoinGecko Notification Manager")
+                                        .title("COING-ECKO PRICE NOTIFICATION")
                                         .text(String.format("The price of %s, is within %.2f and %.2f\nPrice: %.2f USD", te.getCoinID(), te.getPriceLowerBound(), te.getPriceUpperBound(), price))
                                         .hideAfter(5000)
                                         .shake(1000, 15)
@@ -44,10 +45,10 @@ public class NotifierDaemon implements Runnable {
                         }
                     }
                 } else {
-                    System.out.println("Notification Manager Disabled");
+                    System.out.println("me disabled");
                 }
             } catch (Exception e) {
-                System.out.println("Exception:");
+                System.out.println("Exception");
                 e.printStackTrace();
             }
         }
